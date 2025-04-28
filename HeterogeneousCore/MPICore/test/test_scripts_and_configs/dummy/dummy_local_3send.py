86d5892680f6765dab7a5be6fa6164a5a79fa16b
Annnnya <anna.polova@ucu.edu.ua>
2025-04-28 09:10:29 +0200

    added tests

--- HeterogeneousCore/MPICore/test/test_scripts_and_configs/dummy/dummy_local_3send.py
+++ HeterogeneousCore/MPICore/test/test_scripts_and_configs/dummy/dummy_local_3send.py
@@ -76,7 +76,7 @@ process.mpiController.run_local = cms.untracked.bool(True)
 
 # Dummy producer
 process.dummyProducer1 = cms.EDProducer("DummyProducer",
-    sizeInBytes=cms.uint32(1024*1024)
+    sizeInBytes=cms.uint32(1024)
 )
 
 process.dummyProducer2 = cms.EDProducer("DummyProducer",
