ed50c14b31975e7625b8a69769d49ce65bd13153
Mario Gonzalez <mario.gonzalez.carpintero@cern.ch>
2025-10-22 10:19:45 +0200

    Introduce TrivialCopyTraits and TrivialSerialiser
    Co-authored-by: Andrea Bocci <andrea.bocci@cern.ch>

--- DataFormats/Common/interface/TrivialCopyTraits.h
+++ DataFormats/Common/interface/TrivialCopyTraits.h
@@ -60,6 +60,17 @@ namespace edm {
   using TrivialCopyProperties = decltype(TrivialCopyTraits<T>::properties(std::declval<T const&>()));
 
   // Checks if the declaration of initialize(...) is consistent with the presence or absence of properties.
+  template <typename T, typename TQueue>
+  concept HasValidInitializeAsync =
+      // does not have properties(...) and initialize(object) takes a single argument
+      (not HasTrivialCopyProperties<T> &&
+       requires(T& object, TQueue& queue) { TrivialCopyTraits<T>::initialize(object, queue); }) ||
+      // or does have properties(...) and initialize(object, props) takes two arguments
+      (HasTrivialCopyProperties<T> && requires(T& object, TQueue& queue, TrivialCopyProperties<T> props) {
+        TrivialCopyTraits<T>::initialize(object, queue, props);
+      });
+
+  // Checks if the declaration of initialize(...) is consistent with the presence or absence of properties.
   template <typename T>
   concept HasValidInitialize =
       // does not have properties(...) and initialize(object) takes a single argument
@@ -83,9 +94,18 @@ namespace edm {
       // and has either no initialize(...) or a valid one
       (not requires { &TrivialCopyTraits<T>::initialize; } || HasValidInitialize<T>);
 
+  template <typename T, typename TQueue>
+  concept HasTrivialCopyTraitsAsync =
+      // Has memory regions declared
+      (HasRegions<T>) &&
+      // and has either no initialize(...) or a valid one
+      (not requires { &TrivialCopyTraits<T>::initialize; } || HasValidInitializeAsync<T, TQueue>);
+
   // Checks if finalize(...) is defined
   template <typename T>
   concept HasTrivialCopyFinalize = requires(T& object) { edm::TrivialCopyTraits<T>::finalize(object); };
+
+
 
   // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   // Specialisations for various types
@@ -94,31 +114,28 @@ namespace edm {
   template <typename T>
     requires std::is_arithmetic_v<T>
   struct TrivialCopyTraits<T> {
-    using value_type = T;
+    static std::vector<std::span<std::byte>> regions(T& object) {
+      return {{reinterpret_cast<std::byte*>(&object), sizeof(T)}};
+    }  // namespace edm
 
-    static std::vector<std::span<std::byte>> regions(value_type& object) {
-      return {{reinterpret_cast<std::byte*>(&object), sizeof(value_type)}};
-    }
-
-    static std::vector<std::span<const std::byte>> regions(value_type const& object) {
-      return {{reinterpret_cast<std::byte const*>(&object), sizeof(value_type)}};
+    static std::vector<std::span<const std::byte>> regions(T const& object) {
+      return {{reinterpret_cast<std::byte const*>(&object), sizeof(T)}};
     }
   };
 
   // Specialisation for std::string
   template <>
   struct TrivialCopyTraits<std::string> {
-    using value_type = std::string;
     using Properties = std::string::size_type;
 
-    static Properties properties(value_type const& object) { return object.size(); }
-    static void initialize(value_type& object, Properties const& size) { object.resize(size); }
+    static Properties properties(std::string const& object) { return object.size(); }
+    static void initialize(std::string& object, Properties const& size) { object.resize(size); }
 
-    static std::vector<std::span<std::byte>> regions(value_type& object) {
+    static std::vector<std::span<std::byte>> regions(std::string& object) {
       return {{reinterpret_cast<std::byte*>(object.data()), object.size() * sizeof(char)}};
     }
 
-    static std::vector<std::span<const std::byte>> regions(value_type const& object) {
+    static std::vector<std::span<const std::byte>> regions(std::string const& object) {
       return {{reinterpret_cast<std::byte const*>(object.data()), object.size() * sizeof(char)}};
     }
   };
@@ -127,17 +144,16 @@ namespace edm {
   template <typename T>
     requires(std::is_arithmetic_v<T> and not std::is_same_v<T, bool>)
   struct TrivialCopyTraits<std::vector<T>> {
-    using value_type = std::vector<T>;
     using Properties = std::vector<T>::size_type;
 
-    static Properties properties(value_type const& object) { return object.size(); }
-    static void initialize(value_type& object, Properties const& size) { object.resize(size); }
+    static Properties properties(std::vector<T> const& object) { return object.size(); }
+    static void initialize(std::vector<T>& object, Properties const& size) { object.resize(size); }
 
-    static std::vector<std::span<std::byte>> regions(value_type& object) {
+    static std::vector<std::span<std::byte>> regions(std::vector<T>& object) {
       return {{reinterpret_cast<std::byte*>(object.data()), object.size() * sizeof(T)}};
     }
 
-    static std::vector<std::span<const std::byte>> regions(value_type const& object) {
+    static std::vector<std::span<const std::byte>> regions(std::vector<T> const& object) {
       return {{reinterpret_cast<std::byte const*>(object.data()), object.size() * sizeof(T)}};
     }
   };
