07f69b73b070c0625e27038c7d0ab2fa816cc176
Annnnya <anna.polova@ucu.edu.ua>
2026-01-06 12:26:01 +0100

    modified config for 3 remotes

---
+++ HeterogeneousCore/MPICore/test/test_scripts_and_configs/real/hlt_remote_hbhe.py
@@ -0,0 +1,185 @@
+import FWCore.ParameterSet.Config as cms
+
+import os
+
+from hlt import process as _process
+
+process = cms.Process("REMOTE")
+
+process.load("Configuration.StandardSequences.Accelerators_cff")
+# process.load("FWCore.MessageLogger.MessageLogger_cfi")
+
+# load the event setup
+for module in _process.psets.keys():
+    setattr(process, module, getattr(_process, module).clone())
+for module in _process.es_sources.keys():
+    setattr(process, module, getattr(_process, module).clone())
+for module in _process.es_producers.keys():
+    setattr(process, module, getattr(_process, module).clone())
+
+
+process.options.numberOfThreads = int(os.environ.get("EXPERIMENT_THREADS", 32))
+process.options.numberOfStreams = int(os.environ.get("EXPERIMENT_STREAMS", 24))
+process.options.numberOfConcurrentLuminosityBlocks = 1
+
+
+# FastTimer output
+experiment_name = os.environ.get("EXPERIMENT_NAME", "unnamed")
+output_dir = os.environ.get("EXPERIMENT_OUTPUT_DIR", "../../test_results/one_time_tests/")
+
+
+process.FastTimerService = _process.FastTimerService.clone()
+process.FastTimerService.writeJSONSummary = True
+process.FastTimerService.jsonFileName=cms.untracked.string(f"{output_dir}/remote_{experiment_name}.json")
+
+process.ThroughputService = _process.ThroughputService.clone()
+# process.ThroughputService.printEventSummary = True
+# process.load("FWCore/Services/Tracer_cfi")
+
+# set up the MPI communication channel
+process.load("HeterogeneousCore.MPIServices.MPIService_cfi")
+process.MPIService.pmix_server_uri = "file:server.uri"
+
+process.source = cms.Source("MPISource",
+  firstRun = cms.untracked.uint32(396102)
+)
+process.maxEvents.input = -1
+
+# receive the raw data over MPI
+process.rawDataCollector = cms.EDProducer("MPIReceiver",
+    upstream = cms.InputTag("source"),
+    instance = cms.int32(1),
+    remote_rank=cms.untracked.int32(0),
+    products = cms.VPSet(cms.PSet(
+    type = cms.string("FEDRawDataCollection"),
+    label = cms.string("")
+    ), cms.PSet(
+        type = cms.string("edm::PathStateToken"),
+        label = cms.string("")
+    ))
+)
+
+
+process.activityFilterRawData = cms.EDFilter("PathStateRelease",
+    state = cms.InputTag("rawDataCollector")
+    )
+
+
+# process.mpiReceiverhltHbheRecoSoAActivity = cms.EDProducer("MPIReceiver",
+#     upstream = cms.InputTag("source"),
+#     instance = cms.int32(3),
+#     products = cms.VPSet(cms.PSet(
+#         type = cms.string("edm::PathStateToken"),
+#         label = cms.string("")
+#     ))
+# )
+
+# process.mpiReceiverParticleFlowActivity = cms.EDProducer("MPIReceiver",
+#     upstream = cms.InputTag("source"),
+#     instance = cms.int32(4),
+#     products = cms.VPSet(cms.PSet(
+#         type = cms.string("edm::PathStateToken"),
+#         label = cms.string("")
+#     ))
+# )
+
+process.mpiReceiverhltHbheRecoSoAAParticleFlowActivity = cms.EDProducer("MPIReceiver",
+    upstream = cms.InputTag("source"),
+    instance = cms.int32(3),
+    remote_rank=cms.untracked.int32(0),
+    products = cms.VPSet(cms.PSet(
+        type = cms.string("edm::PathStateToken"),
+        label = cms.string("")
+    ))
+)
+
+process.activityFilterRecoParticleFlow = cms.EDFilter("PathStateRelease",
+    state = cms.InputTag("mpiReceiverhltHbheRecoSoAAParticleFlowActivity")
+    )
+
+
+process.hltGetRaw = _process.hltGetRaw.clone()
+
+# HBHE local reconstruction from the HLT menu
+process.hltHcalDigis = _process.hltHcalDigis.clone()
+process.hltHcalDigisSoA = _process.hltHcalDigisSoA.clone()
+process.hltHbheRecoSoA = _process.hltHbheRecoSoA.clone()
+
+process.hltParticleFlowRecHitHBHESoA = _process.hltParticleFlowRecHitHBHESoA.clone()
+process.hltParticleFlowClusterHBHESoA = _process.hltParticleFlowClusterHBHESoA.clone()
+
+# send the HBHE rechits SoA over MPI
+process.mpiSenderHbheRecoSoA = cms.EDProducer("MPISender",
+    upstream = cms.InputTag("rawDataCollector"),
+    instance = cms.int32(11),
+    remote_rank=cms.untracked.int32(0),
+    products = cms.vstring(
+        "128falsehcalHcalRecHitSoALayoutPortableHostCollection_hltHbheRecoSoA__*",
+        "ushort_hltHbheRecoSoA_backend_*",
+        "*_HBHEActivity__*",
+    ) 
+)
+
+# send the HBHE PF rechits SoA over MPI
+process.mpiSenderParticleFlowRecHitHBHESoA = cms.EDProducer("MPISender",
+    upstream = cms.InputTag("mpiSenderHbheRecoSoA"),
+    instance = cms.int32(12),
+    remote_rank=cms.untracked.int32(0),
+    products = cms.vstring(
+        "128falserecoPFRecHitSoALayoutPortableHostCollection_hltParticleFlowRecHitHBHESoA__*",
+        "ushort_hltParticleFlowRecHitHBHESoA_backend_*",
+        "*_HBHEActivity__*",
+    )
+)
+
+# send the HBHE PF clusters SoA over MPI
+process.mpiSenderParticleFlowClusterHBHESoA = cms.EDProducer("MPISender",
+    upstream = cms.InputTag("mpiSenderParticleFlowRecHitHBHESoA"),
+    instance = cms.int32(13),
+    remote_rank=cms.untracked.int32(0),
+    products = cms.vstring(
+        "128falserecoPFClusterSoALayoutPortableHostCollection_hltParticleFlowClusterHBHESoA__*",
+        "128falserecoPFRecHitFractionSoALayoutPortableHostCollection_hltParticleFlowClusterHBHESoA__*",
+        "ushort_hltParticleFlowClusterHBHESoA_backend_*",
+        "*_HBHEActivity__*",
+    )
+)
+
+process.HBHEActivity = cms.EDProducer("PathStateCapture")
+
+# run the HBHE local reconstruction
+process.HLTLocalHBHE = cms.Path(
+    process.activityFilterRawData +
+    process.activityFilterRecoParticleFlow +
+    process.hltGetRaw +
+    process.hltHcalDigis +
+    process.hltHcalDigisSoA +
+    process.hltHbheRecoSoA +
+    process.hltParticleFlowRecHitHBHESoA +
+    process.hltParticleFlowClusterHBHESoA +
+    process.HBHEActivity
+)
+
+process.MPIPath = cms.Path(
+    process.rawDataCollector +
+    # process.mpiReceiverEcalDigisActivity +
+    process.mpiReceiverhltHbheRecoSoAAParticleFlowActivity +
+    # process.mpiReceiverPixelActivity +
+    # process.mpiSenderEcalDigisSoA +
+    # process.mpiSenderEcalUncalibRecHitSoA +
+    process.mpiSenderHbheRecoSoA +
+    process.mpiSenderParticleFlowRecHitHBHESoA +
+    process.mpiSenderParticleFlowClusterHBHESoA
+    # process.mpiSenderSiPixelClustersSoA +
+    # process.mpiSenderSiPixelRecHitsSoA +
+    # process.mpiSenderPixelTracksSoA +
+    # process.mpiSenderPixelVerticesSoA
+)
+
+# schedule the reconstruction
+process.schedule = cms.Schedule(
+    process.HLTLocalHBHE,
+    process.MPIPath
+)
+
+# process.Tracer = cms.Service("Tracer")
