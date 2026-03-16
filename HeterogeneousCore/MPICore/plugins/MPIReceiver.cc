2a69aa139b9605aed8e233403319d9fad3da3503
Annnnya <anna.polova@ucu.edu.ua>
2026-03-16 15:51:01 +0100

    made path state a sepatate parameter and integrated it with metadata

--- HeterogeneousCore/MPICore/plugins/MPIReceiver.cc
+++ HeterogeneousCore/MPICore/plugins/MPIReceiver.cc
@@ -9,6 +9,7 @@
 #include <TClass.h>
 
 // CMSSW include files
+#include "DataFormats/Common/interface/PathStateToken.h"
 #include "FWCore/Concurrency/interface/Async.h"
 #include "FWCore/Concurrency/interface/chain_first.h"
 #include "FWCore/Framework/interface/Event.h"
@@ -34,7 +35,8 @@ public:
   MPIReceiver(edm::ParameterSet const& config)
       : upstream_(consumes<MPIToken>(config.getParameter<edm::InputTag>("upstream"))),
         token_(produces<MPIToken>()),
-        instance_(config.getParameter<int32_t>("instance"))  //
+        instance_(config.getParameter<int32_t>("instance")),
+        activity_(config.getParameter<bool>("activity"))
   {
     // instance 0 is reserved for the MPIController / MPISource pair
     // instance values greater than 255 may not fit in the MPI tag
@@ -42,6 +44,10 @@ public:
       throw cms::Exception("InvalidValue")
           << "Invalid MPIReceiver instance value, please use a value between 1 and 255";
     }
+
+    // if (activity_) {
+      pathStateToken_ = produces<edm::PathStateToken>();
+    // }
 
     auto const& products = config.getParameter<std::vector<edm::ParameterSet>>("products");
     products_.reserve(products.size());
@@ -85,6 +91,8 @@ public:
     if (received_meta_->productCount() == -1) {
       event.emplace(token_, token);
       return;
+    } else {
+      event.emplace(pathStateToken_);
     }
 
     std::unique_ptr<TBufferFile> serialized_buffer;
@@ -168,6 +176,8 @@ public:
         ->setComment("Products to be received by a separate CMSSW job and produced into the event.");
     desc.add<int32_t>("instance", 0)
         ->setComment("A value between 1 and 255 used to identify a matching pair of \"MPISender\"/\"MPIReceiver\".");
+    desc.add<bool>("activity", false)
+      ->setComment("Whether this receiver will get activity information from the sender.");
 
     descriptions.addWithDefaultLabel(desc);
   }
@@ -183,8 +193,11 @@ private:
   edm::EDPutTokenT<MPIToken> const token_;  // copy of the MPIToken that may be used to implement an ordering relation
   std::vector<Entry> products_;             // data to be read over the channel and put into the Event
   int32_t const instance_;                  // instance used to identify the source-destination pair
+  bool activity_;                           // indicator whether the PathStateToken will be received by the module
+  edm::EDPutTokenT<edm::PathStateToken> pathStateToken_;
 
   std::shared_ptr<ProductMetadataBuilder> received_meta_;
+
 };
 
 #include "FWCore/Framework/interface/MakerMacros.h"
